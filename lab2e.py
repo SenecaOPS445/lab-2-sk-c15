3b55c17c33469a2fc2cd75e1a22c8fa3a81577f7
Sandip K C <sk-c15@myseneca.ca>
2024-05-24 14:34:36 -0400

    added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Sandip K C
+#Author ID: sk-c15
+#Date Created: 2024/05/23
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
